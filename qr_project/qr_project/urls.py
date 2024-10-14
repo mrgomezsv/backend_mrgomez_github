ecb23cc611487c87a2e5c5caee516b3b0e92cc01
Mario Roberto <mrgomez.dev@gmail.com>
2024-10-13 23:49:07 -0600

    Agrege las en las rutas de proyectos a accounts

--- qr_project/qr_project/urls.py
+++ qr_project/qr_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('qr/', include('qr_tracker.urls')),  # Incluye las URLs de qr_tracker
     path('', views.home),  # Página de inicio en la raíz
     path('', include('qr_tracker.urls')),
+    path('accounts/', include('accounts.urls')),  # Añadir autenticación
 ]
