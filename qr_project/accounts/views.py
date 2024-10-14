9290cf871ff116bf7d6fcb9b570cd4ccd3b4a6c9
Mario Roberto <mrgomez.dev@gmail.com>
2024-10-13 23:50:18 -0600

    Agrege el codigo de views

--- qr_project/accounts/views.py
+++ qr_project/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# accounts/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Iniciar sesión automáticamente después del registro
+            messages.success(request, f'Tu cuenta ha sido creada. ¡Bienvenido {user.username}!')
+            return redirect('home')  # Redirigir a la página de inicio
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
