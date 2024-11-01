1e31050f816d4f52c2077c2dca9ffa9ba7a3aa9e
Mario Roberto <mrgomez.dev@gmail.com>
2024-10-31 19:00:00 -0600

    configuracion de login con stilos

--- qr_project/settings.py
+++ qr_project/settings.py
@@ -133,7 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Asegúrate de que esta línea esté presente
+]
+
 
 # Para producción, recolecta archivos estáticos en esta carpeta:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
